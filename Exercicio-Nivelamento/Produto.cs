80a51be94aef4f9d6b25e9537e6bc5b254241a37
Roberta Tatiana <robertatatiana00@gmail.com>
2023-08-09 14:03:20 -0300

    Primeiro commit

---
+++ Exercicio-Nivelamento/Produto.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Nivelamento
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }   
+        public int Quantidade { get; set; }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            if (quantidade <= Quantidade)
+            {
+                Quantidade -= quantidade;
+            }
+            else
+            {
+                Console.WriteLine("Quantidade insuficiente em estoque.");
+            }
+        }
+
+        public void MostrarDadosDoProduto()
+        {
+            Console.WriteLine("Dados do Produto:");
+            Console.WriteLine("Nome: " + Nome);
+            Console.WriteLine("Preço: " + Preco);
+            Console.WriteLine("Quantidade em Estoque: " + Quantidade);
+            Console.WriteLine("Valor Total em Estoque: " + ValorTotalEmEstoque());
+        }
+
+    }
+}
---
+++ Exercicio-Nivelamento/Program.cs
@@ -0,0 +1,32 @@
+﻿using Exercicio_Nivelamento;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Digite o nome do produto: ");
+        string nome = Console.ReadLine()!;
+
+        Console.Write("Digite o preço do produto: ");
+        double preco = double.Parse(Console.ReadLine()!);
+
+        Console.Write("Digite a quantidade em estoque: ");
+        int quantidade = int.Parse(Console.ReadLine()!);
+
+        Produto produto = new Produto(nome, preco, quantidade);
+
+        produto.MostrarDadosDoProduto();
+
+        Console.Write("Digite a quantidade a ser adicionada ao estoque: ");
+        int quantidadeAdicionar = int.Parse(Console.ReadLine()!);
+        produto.AdicionarProdutos(quantidadeAdicionar);
+
+        produto.MostrarDadosDoProduto();
+
+        Console.Write("Digite a quantidade a ser removida do estoque: ");
+        int quantidadeRemover = int.Parse(Console.ReadLine()!);
+        produto.RemoverProdutos(quantidadeRemover);
+
+        produto.MostrarDadosDoProduto();
+    }
+}
